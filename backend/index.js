2da0b8b78fc5427765c4b6005480804b83ae5929
Pedro Ramos <ramuspedro@gmail.com>
2018-08-21 21:35:27 -0300

    working with path

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,22 @@
 
 // Dependencies
 const http = require('http')
+const url = require('url')
 
 // The server should respond to all the requests with a string
 const server = http.createServer((req, res, next) => {
+  // Get the URL and parsed
+  let parsedUrl = url.parse(req.url, true)
+  
+  // Get the path
+  let path = parsedUrl.pathname
+  let trimmedPath = path.replace(/^\/+|\/+$/g, '')
+
+  // Send the response
   res.end('Hello World!\n')
+  
+  // Log the request path
+  console.log("Request is received on path: ", trimmedPath);
 })
 
 // start the server, and have it listen on port 3000
