5fe78910f104107c5381a99be41864b4ae2f1516
Pedro Ramos <ramuspedro@gmail.com>
2018-08-22 21:30:49 -0300

    getting the payload

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@
 // Dependencies
 const http = require('http')
 const url = require('url')
+const stringDecoder = require('string_decoder').StringDecoder
 
 // The server should respond to all the requests with a string
 const server = http.createServer((req, res, next) => {
@@ -24,15 +25,24 @@ const server = http.createServer((req, res, next) => {
   // Get the headers as an object
   let headers = req.headers
 
-  // Send the response
-  res.end('Hello World!\n')
-  
-  // Log the request path
-  console.log("*************");
-  console.log("Request is received on path: ", trimmedPath);
-  console.log("Method: ", method);
-  console.log("Query: ", queryStringObject);
-  console.log("Headers: ", headers);
+  // Get the payload, if any
+  let decoder = new stringDecoder('utf-8')
+  let buffer = ''
+  req.on('data', (data) => {
+    buffer += decoder.write(data)
+  })
+  req.on('end', () => {
+    buffer += decoder.end()
+
+    // Send the response
+    res.end('Hello World!\n')
+    
+    // Log the request path
+    console.log("*************");
+    console.log("Payload: ", buffer);
+
+  })
+
 })
 
 // start the server, and have it listen on port 3000
